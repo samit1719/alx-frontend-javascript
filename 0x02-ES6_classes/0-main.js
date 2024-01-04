626e1e6d926a59ac399a3ec5e4999e0273ddb629
samit1719 <samuelteshome1719@gmail.com>
2024-01-04 22:42:25 +0300

    updated advancedtask and main

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -94,17 +94,27 @@
 // console.log(listPrinted)
 
 // 10
-import Car from "./10-car.js";
+// import Car from "./10-car.js";
 
-class TestCar extends Car {}
+// class TestCar extends Car {}
 
-const tc1 = new TestCar("Nissan", "Turbo", "Pink");
-const tc2 = tc1.cloneCar();
+// const tc1 = new TestCar("Nissan", "Turbo", "Pink");
+// const tc2 = tc1.cloneCar();
 
-console.log(tc1);
-console.log(tc1 instanceof TestCar);
+// console.log(tc1);
+// console.log(tc1 instanceof TestCar);
 
-console.log(tc2);
-console.log(tc2 instanceof TestCar);
+// console.log(tc2);
+// console.log(tc2 instanceof TestCar);
 
-console.log(tc1 == tc2);
+// console.log(tc1 == tc2);
+
+// 100
+import EVCar from './100-evcar.js';
+
+const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
+console.log(ec1);
+
+const ec2 = ec1.cloneCar();
+console.log(ec2);
+
